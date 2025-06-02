e7888af4ccd6636397c01885ee04ef7688eca78c
ThinhNguyenPhong <phongthinh799@gmail.com>
2025-06-02 14:35:39 +0700

    fix-db

--- HIVTreatmentSystem/HIVTreatmentSystem.Application/Services/Auth/AuthenticateService.cs
+++ HIVTreatmentSystem/HIVTreatmentSystem.Application/Services/Auth/AuthenticateService.cs
@@ -1,10 +1,10 @@
-﻿using HIVTreatmentSystem.Application.Interfaces;
-using HIVTreatmentSystem.Domain.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HIVTreatmentSystem.Application.Interfaces;
+using HIVTreatmentSystem.Domain.Interfaces;
 
 namespace HIVTreatmentSystem.Application.Services.Auth
 {
@@ -13,7 +13,10 @@ namespace HIVTreatmentSystem.Application.Services.Auth
         private readonly IAccountRepository _accountRepository;
         private readonly IPasswordHasher _passwordHasher;
 
-        public AuthenticateService(IAccountRepository accountRepository, IPasswordHasher passwordHasher)
+        public AuthenticateService(
+            IAccountRepository accountRepository,
+            IPasswordHasher passwordHasher
+        )
         {
             _accountRepository = accountRepository;
             _passwordHasher = passwordHasher;
@@ -24,10 +27,14 @@ namespace HIVTreatmentSystem.Application.Services.Auth
             try
             {
                 var account = await _accountRepository.GetByIdAsync(id);
-                bool checkPassword = _passwordHasher.VerifyPassword(oldPassword, account.PasswordHash);
-                if (!checkPassword) return false;
+                bool checkPassword = _passwordHasher.VerifyPassword(
+                    oldPassword,
+                    account.PasswordHash
+                );
+                if (!checkPassword)
+                    return false;
                 account.PasswordHash = _passwordHasher.HashPassword(newPassword);
-                await _accountRepository.UpdateAsync(account);
+                _accountRepository.Update(account);
                 return true;
             }
             catch (Exception ex)
