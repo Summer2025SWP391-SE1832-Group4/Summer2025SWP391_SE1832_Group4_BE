2e98048ffd80b9cc98024dbc9871fe23c7149748
Nguyen Duc Tuan <tuanndse173318@fpt.edu.vn>
2025-06-09 00:19:21 +0700

    UPDATE FIX EXPERIENCE WORKING AND LIST DOCTOR BY SLOTS

--- HIVTreatmentSystem/HIVTreatmentSystem.Domain/Interfaces/IGenericRepository.cs
+++ HIVTreatmentSystem/HIVTreatmentSystem.Domain/Interfaces/IGenericRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace HIVTreatmentSystem.Domain.Interfaces
 {
     public interface IGenericRepository<T, TKey> where T : class
     {
-        Task<T> GetByIdAsync(TKey id);
+        Task<T?> GetByIdAsync(TKey id);
         Task<IEnumerable<T>> GetAllAsync();
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression);
         Task AddAsync(T entity);
